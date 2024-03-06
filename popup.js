f7ffd0ba1728f5290f23f07eefb686390011ee9b
GarbageHaus <90212322+GarbageHaus@users.noreply.github.com>
2024-03-06 15:50:25 -0500

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,104 @@
+// Forked from https://github.com/joeperpetua/simple-js-injector/blob/main/popup.js
+// Storage Documentation: https://developer.chrome.com/extensions/storage
+document.addEventListener('DOMContentLoaded', function() {
+  var urlTextarea = document.getElementById('url');
+  var codeTextarea = document.getElementById('jscode');
+  var injectButton = document.getElementById('inject');
+  var importButton = document.getElementById('import'); //added new elements to HTML
+  var exportButton = document.getElementById('export');
+
+  chrome.tabs.query({active: true},function(tab) {
+    var tabURL = tab[0].url;
+
+    chrome.storage.sync.get(null, function(theValue) {
+      var url = getLongestURLMatch(theValue, tabURL);
+      urlTextarea.value = url;
+      if (url.length > 0) {
+        urlTextarea.value = url;
+        codeTextarea.value = theValue[url];
+      } else {
+        urlTextarea.value = tabURL;
+        codeTextarea.value = '';
+      }
+    });
+
+    injectButton.addEventListener('click', function() {
+      var theValue = codeTextarea.value;
+      if (!theValue) {
+        chrome.storage.sync.remove(urlTextarea.value, function() {
+          // Notify that we saved.
+          console.log('REMOVED script for ' + urlTextarea.value);
+		  //chrome.extension.getBackgroundPage().console.log('foo'); Logging no longer works in Failfox.
+        });
+        window.close();
+        return;
+      }
+
+      chrome.tabs.executeScript({
+        code: theValue
+      });
+
+      // Save it using the Chrome extension storage API.
+      var keypair = {};
+      keypair[urlTextarea.value] = theValue;
+      chrome.storage.sync.set(keypair, function() {
+        // Notify that we saved.
+        console.log('SAVED ' + JSON.stringify(keypair));
+      });
+      window.close();
+    });
+	
+	//Function above was copied to make import function here.
+	importButton.addEventListener('click', function() {
+	var inputElement = document.createElement('input');
+	inputElement.type = 'file';
+	inputElement.accept = '.json';
+	inputElement.onchange = function() {
+		var file = inputElement.files[0];
+		var reader = new FileReader();
+		reader.onload = function() {
+			var jsonContent = JSON.parse(reader.result);
+			var keypair = {};
+			for (var i = 0; i< jsonContent.length; i++) {
+				keypair[jsonContent[i]] = theValue;
+			}
+			chrome.storage.sync.set(keypair, function() {
+				console.log("Saved the keypair: " + JSON.stringify(keypair));
+				
+			});
+			//maybe works? Does actually return SOMETHING
+			chrome.storage.sync.get(null, function(items) {
+			var allKeys = Object.keys(items);
+			console.log(allKeys);
+			});
+			
+		};
+		reader.readAsText(file);
+	};
+	inputElement.click();
+    });
+	
+	//Export files: WORKS!
+	exportButton.addEventListener('click', function() {
+		var key = urlTextarea.value;
+	
+		chrome.storage.sync.get(null, function(items) {
+		// Convert the items to a JSON string
+		var itemsString = JSON.stringify(items);
+		
+		// Convert the JSON string to a Blob
+		var blob = new Blob([itemsString], {type: "application/json"});
+		
+		var a = document.createElement('a');
+		a.href = URL.createObjectURL(blob);
+		a.download = "allData.json";
+		document.body.appendChild(a);    
+		a.click();    
+		document.body.removeChild(a);
+			setTimeout(function() {
+			window.close();
+			}, 200);
+		});
+	});
+  });
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+## Simple JS Injector
+
+This addon creates a small popup that lets you paste some JS code and inject it into the page you are currently seeing. It also allows for Importing and Exporting JSON files so that customizations can be shared within your organization or group without requiring it to be public.
+
+This is a clone of the respository from [joeperpetua](https://github.com/joeperpetua/simple-js-injector) with the express purpose of adding the import/export functionality. This respository is seperate to allow for additional changes.
+
+This, _originally_ is an adaptation to Firefox version of the Chrome extension "JS injector" from  [Daniel Han](https://github.com/hex0cter/js-injector). It is [only available in Chrome](https://chrome.google.com/webstore/detail/javascript-injector/ejnccfcackblkelbafbolcpjfpcmbplg) it has been updated by [joeperpetua] to work in Firefox.
+
+You can check out the original GitHub page for Chrome [here](https://github.com/hex0cter)
+The link to the Firefox addon can be found [here](https://addons.mozilla.org/firefox/addon/simple-javascript-injector/)
+
+#Addon Repo
+This space will be used to provide a link to this repo's addon when the changes have been completed. (https://addons.mozilla.org/)
+
